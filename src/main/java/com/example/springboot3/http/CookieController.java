e21fc8e960348859ffe3137185056a416bd3e34a
chanhl22 <lch9502@naver.com>
2024-12-06 14:44:17 +0900

    feat: IntelliJ의 .http를 사용해 Postman 대체하기

--- src/main/java/com/example/springboot3/http/CookieController.java
+++ src/main/java/com/example/springboot3/http/CookieController.java
@@ -31,6 +31,26 @@ public class CookieController {
         return "Cookie has been set!";
     }
 
+    @PostMapping("/login/set-cookies")
+    public String login2(@RequestBody UserRequest request, HttpServletResponse response) {
+        log.info("login request: {}", request);
+
+        // 쿠키 생성
+        Cookie cookie1 = new Cookie("myCookie1", "good");
+        Cookie cookie2 = new Cookie("myCookie2", "soso");
+        Cookie cookie3 = new Cookie("myCookie3", "bad");
+        cookie1.setMaxAge(60 * 60 * 24);
+        cookie2.setMaxAge(60 * 60 * 24);
+        cookie3.setMaxAge(60 * 60 * 24);
+
+        // 응답에 쿠키 추가
+        response.addCookie(cookie1);
+        response.addCookie(cookie2);
+        response.addCookie(cookie3);
+
+        return "Cookies has been set!";
+    }
+
     @GetMapping("/find/get-cookie")
     public String find(HttpServletRequest request) {
         if (request.getCookies() != null) {
@@ -40,6 +60,17 @@ public class CookieController {
         }
 
         return "Cookie has been get!";
+    }
+
+    @GetMapping("/find/get-cookies")
+    public String find2(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                log.info("cookie.getName(): {}, cookie.getValue(): {}", cookie.getName(), cookie.getValue());
+            }
+        }
+
+        return "Cookies has been get!";
     }
 
 }
